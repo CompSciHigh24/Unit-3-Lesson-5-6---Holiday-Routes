f0c40526c00c1012d2b8668da924b0770cb2150a
CompSciHigh24 <csh-github@compscihigh.org>
2025-12-17 16:17:40 +0000

    template

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
 
 app.listen(3000, () => {
-  console.log(`Holiday Server is Running!`);
+  console.log(`Holiday Server is Running!`)
 });
